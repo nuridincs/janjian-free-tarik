dba08a36186d1c4febc5fafe15aaefd3c6c55aef
Muhammad Nuridin <muhammad.nuridin@rajamobil.net>
2020-02-15 22:05:55 +0700

    export db

--- config/db_janjian_free_tarik_2020-02-15.sql
+++ config/db_janjian_free_tarik_2020-02-15.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.7.27)
 # Database: db_janjian_free_tarik
-# Generation Time: 2020-02-15 10:35:00 +0000
+# Generation Time: 2020-02-15 15:03:40 +0000
 # ************************************************************
 
 
@@ -40,6 +40,80 @@ VALUES
 	(2,'user123');
 
 /*!40000 ALTER TABLE `app_account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table app_tarik
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `app_tarik`;
+
+CREATE TABLE `app_tarik` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_time` int(11) DEFAULT NULL,
+  `name_line` varchar(50) DEFAULT NULL,
+  `date_label` varchar(20) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `time` (`id_time`),
+  CONSTRAINT `time` FOREIGN KEY (`id_time`) REFERENCES `app_time` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `app_tarik` WRITE;
+/*!40000 ALTER TABLE `app_tarik` DISABLE KEYS */;
+
+INSERT INTO `app_tarik` (`id`, `id_time`, `name_line`, `date_label`, `date`)
+VALUES
+	(11,14,'ncs','today','2020-02-14'),
+	(12,22,'ncs 13','today','2020-02-15'),
+	(13,1,'zibran','tomorrow','2020-02-16');
+
+/*!40000 ALTER TABLE `app_tarik` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table app_time
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `app_time`;
+
+CREATE TABLE `app_time` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` char(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `app_time` WRITE;
+/*!40000 ALTER TABLE `app_time` DISABLE KEYS */;
+
+INSERT INTO `app_time` (`id`, `label`)
+VALUES
+	(1,'00.00'),
+	(2,'01.00'),
+	(3,'02.00'),
+	(4,'03.00'),
+	(5,'04.00'),
+	(6,'05.00'),
+	(7,'06.00'),
+	(8,'07.00'),
+	(9,'08.00'),
+	(10,'09.00'),
+	(11,'10.00'),
+	(12,'11.00'),
+	(13,'12.00'),
+	(14,'13.00'),
+	(15,'14.00'),
+	(16,'15.00'),
+	(17,'16.00'),
+	(18,'17.00'),
+	(19,'18.00'),
+	(20,'19.00'),
+	(21,'20.00'),
+	(22,'21.00'),
+	(23,'22.00'),
+	(24,'23.00');
+
+/*!40000 ALTER TABLE `app_time` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
