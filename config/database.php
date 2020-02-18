72938399abb25c95030afe046dc021e2a78b323b
Muhammad Nuridin <muhammad.nuridin@rajamobil.net>
2020-02-19 06:56:24 +0700

    modify db config

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 date_default_timezone_set("Asia/Bangkok");
 
-//Set config db local
-$db_handle = mysqli_connect("localhost","root","","db_janjian_free_tarik");
-
 if ($_SERVER['SERVER_NAME'] != 'localhost') {
   //Set config db production
   $db_handle = mysqli_connect("localhost", "jalanklu_root", "jalankluar2020", "jalanklu_janjian-free-tarik");
+} else {
+  //Set config db local
+  $db_handle = mysqli_connect("localhost","root","","db_janjian_free_tarik");
 }
 
 // Check connection
